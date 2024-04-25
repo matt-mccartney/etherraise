167c125c8b6ddc47e7b3376b20585e9492a25164
Matt McCartney <mlm071604@hotmail.com>
2024-04-25 08:39:30 -0400

    Environment setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { League_Spartan } from "next/font/google";
+
+const leagueSpartan = League_Spartan({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={leagueSpartan.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
