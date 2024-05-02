67ef021012f679856da63413f0f6167aa12347c7
Matt McCartney <mlm071604@hotmail.com>
2024-05-01 23:31:43 -0400

    Update styling for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+// @ts-nocheck
+import { Web3Provider } from "@/components/Web3Auth/Web3Context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
 import { League_Spartan } from "next/font/google";
 
-const leagueSpartan = League_Spartan({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
+const leagueSpartan = League_Spartan({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={leagueSpartan.className}>
-      <Component {...pageProps} />
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
     </main>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,59 @@
 import { CTAButton } from "@/components/common";
 import Navbar from "@/components/Navbar/Navbar";
 import Showcase from "@/components/Showcase/Showcase";
+import { BrowserProvider, ethers } from "ethers";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import tw from "tailwind-styled-components";
+import TokenRaise from "@/contracts/TokenRaise.json";
 
-const PageSection = tw.div<any>`max-w-[1200px] mx-auto py-8`
+const PageSection = tw.div<any>`max-w-[1200px] mx-auto py-8`;
 
 export default function Home() {
   const router = useRouter();
+  const [campaigns, setCampaigns] = useState([])
+
+  useEffect(() => {
+    let fetchCampaigns = async () => {
+      if (!window.ethereum) return;
+      const provider = new BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const tokenRaise = new ethers.Contract(
+        TokenRaise.address,
+        TokenRaise.abi,
+        signer
+      );
+      return tokenRaise.getAllCampaigns()
+    };
+
+    fetchCampaigns().then(x => console.log(x))
+  }, []);
 
   return (
     <>
       <main>
-        <Navbar/>
+        <Navbar />
         <section className="p-8 py-16 justify-center text-center">
-          <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold mb-2">Your <span className="text-violet-500">decentralized</span> fundraising home.</h1>
-          <h3 className="text-gray-200 mb-8">TokenLaunch is where early adopters fund the future, one token at a time.</h3>
-          <CTAButton onClick={() => router.push("/#campaigns")}>View Campaigns</CTAButton>
+          <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold mb-2">
+            Your <span className="text-violet-500">decentralized</span>{" "}
+            fundraising home.
+          </h1>
+          <h3 className="text-gray-200 mb-8">
+            TokenLaunch is where early adopters fund the future, one token at a
+            time.
+          </h3>
+          <CTAButton onClick={() => router.push("/#campaigns")}>
+            View Campaigns
+          </CTAButton>
         </section>
         <PageSection>
-          <Showcase data={[]}/>
+          <Showcase data={[]} />
         </PageSection>
         <PageSection id="campaigns">
-          <h1 className="text-white text-3xl font-semibold">Popular Projects</h1>
+          <h1 className="text-white text-3xl font-semibold">
+            Popular Projects
+          </h1>
         </PageSection>
         <PageSection></PageSection>
         <PageSection></PageSection>
